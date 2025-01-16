b7eb420913be592b67d21616cdfa4f7a5d210c25
swami-dev <blockswitching@gmail.com>
2025-01-16 18:13:59 +0000

    user view in django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import User  # Import your custom User model
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Optionally, customize the admin view for the User model
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
